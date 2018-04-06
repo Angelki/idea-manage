4c0e1f0243b870f1cd65fe29bf754668700a2bdf
zzz <angelks4@live.com>
2018-04-06 14:47:46 +0800

    add ideas html

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const mongoose = require("mongoose");
 const app = express();
 
 // Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
 // connect to mongoose
 mongoose
-  .connect("mongodb://localhost/aideas-dev", {
+  .connect("mongodb://localhost/aideasDev", {
     useMongoClient: true
   })
   .then(() => console.log("Mongodb connected..."))
   .catch(err => console.log(err));
+
+// load Idea model
+require("./models/Idea");
+const Idea = mongoose.model("ideas");
 
 //handlebars middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -29,8 +34,15 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+// add idea
+app.get("/ideas/add", (req, res) => {
+  res.render("ideas/add");
+});
+
 const port = 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
 });
+
+// mongod.exe --logpath C:\mongodb\log\mongo.log --logappend --dbpath C:\mongodb\data\db --serviceName MongoDB --journal --install
