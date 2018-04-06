135e45ff689a38655bb767f3d5d9dadfc52c531f
zzz <angelks4@live.com>
2018-04-06 15:12:16 +0800

    如果没输入就报错

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -22,6 +23,10 @@ const Idea = mongoose.model("ideas");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   const title = "Welcome2";
   res.render("index", {
@@ -37,6 +42,26 @@ app.get("/about", (req, res) => {
 // add idea
 app.get("/ideas/add", (req, res) => {
   res.render("ideas/add");
+});
+
+// process form
+app.post("/ideas", (req, res) => {
+  let errors = [];
+  if (!req.body.title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!req.body.details) {
+    errors.push({ text: "Please add some details" });
+  }
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send(`passed`);
+  }
 });
 
 const port = 5000;
