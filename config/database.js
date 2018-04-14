e0d7a02c13a17d675d611d71b7b3c8f1a64340e2
zzz <angelks4@live.com>
2018-04-14 11:17:44 +0800

    'bug'

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    mongoURI: " mongodb:zzz:zzz123456@ds014388.mlab.com:14388/idea-manage-prod"
+    mongoURI: "mongodb:zzz:zzz123456@ds014388.mlab.com:14388/idea-manage-prod"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/aideas-dev" };
